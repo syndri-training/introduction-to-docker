178bfc5436eb3471c79ec1d9f1609c5b8d00a3ff
bhague1281 <bhague1281@gmail.com>
2017-01-28 11:32:33 -0800

    code for data volumes

---
+++ data-volumes/randomNumber.js
@@ -0,0 +1,14 @@
+// Generates a random number and stores the value in a variable
+const randomNumber = Math.floor(Math.random() * 20);
+
+// Output the random number to the console
+console.log(randomNumber);
+
+//
+// The two lines below, when uncommented, will write the random number to
+// a text file using the 'fs' module (short for filesystem). This file will
+// be written to the host directory you specified when bind-mounting the volume
+//
+
+// const fs = require('fs');
+// fs.writeFileSync('random-number.txt', randomNumber);
