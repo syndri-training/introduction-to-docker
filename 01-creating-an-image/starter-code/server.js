942e81fef81584705775db2d047a7fc2a06f3adb
bhague1281 <bhague1281@gmail.com>
2017-01-28 13:53:41 -0800

    removed old starter code

--- 01-creating-an-image/starter-code/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('express')();
-
-app.get('/', (req, res) => {
-  res.send(`<h1>A-O RIVER!</h1>
-            <img src="http://i.giphy.com/26tkn6aLlSimb9azS.gif" alt="A-O River!" />`);
-});
-
-app.listen(process.env.PORT);
